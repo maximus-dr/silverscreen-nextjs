ab1c8550982e0c0a4a70eee21c483ec18551dbb1
m.ivanov <M.ivanov@cinemalab.io>
2021-06-25 17:25:35 +0300

    Added Checkbox component

--- components/index.js
+++ components/index.js
@@ -1,9 +1,11 @@
 import Button from "./Button/Button";
+import Checkbox from "./Checkbox/Checkbox";
 import Label from "./Label/Label";
 
 
 
 export const Provider = {
     'button': Button,
+    'checkbox': Checkbox,
     'label': Label,
 }
