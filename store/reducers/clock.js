fdcead72de86ead263ce9091074db7aa7dd3c2df
m.ivanov <M.ivanov@cinemalab.io>
2021-06-29 14:51:56 +0300

    Connected with redux

---
+++ store/reducers/clock.js
@@ -0,0 +1,35 @@
+const initialState = {
+    lastUpdate: 0,
+    light: false,
+    count: 0
+}
+
+const clockReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case 'TICK':
+            return {
+                ...state,
+                lastUpdate: action.lastUpdate,
+                light: !!action.light
+            }
+        case 'INCREMENT':
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case 'DECREMENT':
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        case 'RESET':
+            return {
+                ...state,
+                count: initialState.count
+            }
+        default:
+            return state
+    }
+}
+
+export default clockReducer;
---
+++ store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import clockReducer from "./clock";
+
+
+const rootReducer = combineReducers({
+    clock: clockReducer
+});
+
+export default rootReducer;
