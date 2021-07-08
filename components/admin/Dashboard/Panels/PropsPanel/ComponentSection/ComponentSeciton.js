5cad64049910b5985647ffe6c00c025c3afb3bf5
m.ivanov <M.ivanov@cinemalab.io>
2021-07-08 14:18:02 +0300

    Modified ComponentSection

--- components/admin/Dashboard/Panels/PropsPanel/ComponentSection/ComponentSeciton.js
+++ components/admin/Dashboard/Panels/PropsPanel/ComponentSection/ComponentSeciton.js
@@ -1,17 +1,78 @@
-import React from 'react'
-import { ComponentData } from './ComponentSectionStyled'
+import React, { useEffect, useState } from 'react'
+import { ComponentData, ComponentElements, ComponentPropItem, ComponentPropKey, ComponentPropValue, ComponentSectionWrapper } from './ComponentSectionStyled'
 
 
 export default function ComponentSeciton(props) {
 
     const {activeComponent} = props;
+    const entries = Object.entries(activeComponent);
 
+
+    const [name, setName] = useState(() => activeComponent.name || '');
+    const [value, setValue] = useState(() => activeComponent.value || '');
+
+    const onNameFieldChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const onValueFieldChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    useEffect(() => {
+        if (activeComponent.name) {
+            setName(activeComponent.name);
+        }
+        return;
+    }, [activeComponent]);
+
+    useEffect(() => {
+        if (activeComponent.value) {
+            setValue(activeComponent.value);
+        }
+        return;
+    }, [activeComponent])
+
+    
     return (
-        <>
+        <ComponentSectionWrapper>
             <ComponentData>
-                <div><b>id:</b> {activeComponent && activeComponent.id}</div>
-                <div><b>name:</b> {activeComponent && activeComponent.name}</div>
+                {entries && entries.map(entrie => {
+                    console.log(entrie);
+                    if (entrie[0] === 'childrenList') return;
+                    if (entrie[0] === 'styles') return;
+                    if (typeof entrie[1] !== 'string') return;
+
+                    if (entrie[0] === 'name') {
+                        return (
+                            <ComponentPropItem key={entrie[0]}>
+                                <ComponentPropKey>{entrie[0]}:</ComponentPropKey>
+                                <ComponentPropValue>
+                                    <input type="text" value={name} onChange={onNameFieldChange} />
+                                </ComponentPropValue>
+                            </ComponentPropItem>
+                        );
+                    }
+
+                    if (entrie[0] === 'value') {
+                        return (
+                            <ComponentPropItem key={entrie[0]}>
+                                <ComponentPropKey>{entrie[0]}:</ComponentPropKey>
+                                <ComponentPropValue>
+                                    <input type="text" value={value} onChange={onValueFieldChange} />
+                                </ComponentPropValue>
+                            </ComponentPropItem>
+                        );
+                    }
+
+                    return (
+                        <ComponentPropItem key={entrie[0]}>
+                            <ComponentPropKey>{entrie[0]}:</ComponentPropKey>
+                            <ComponentPropValue>{entrie[1]}</ComponentPropValue>
+                        </ComponentPropItem>
+                    );
+                })}
             </ComponentData>
-        </>
+        </ComponentSectionWrapper>
     )
 }
--- components/admin/Dashboard/Panels/PropsPanel/ComponentSection/ComponentSectionStyled.js
+++ components/admin/Dashboard/Panels/PropsPanel/ComponentSection/ComponentSectionStyled.js
@@ -1,8 +1,33 @@
 import styled from "styled-components";
 
+export const ComponentSectionWrapper = styled.div`
+
+`;
+
 export const ComponentData = styled.div`
     width: 100%;
-    padding: 5px;
     margin-bottom: 10px;
     border: 1px dashed black;
+    padding: 5px 0;
+`;
+
+export const ComponentPropItem = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 2px 5px;
+`;
+
+export const ComponentPropKey = styled.span`
+    display: inline-block;
+    padding-right: 5px;
+    font-weight: bold;
+`;
+
+export const ComponentPropValue = styled.span`
+    display: inline-block;
+`;
+
+export const ComponentElements = styled.div`
+    border: 1px dashed black;
+    padding: 5px;
 `;
