723bda307ac66efb766f8fd26c001ef65c592f87
m.ivanov <M.ivanov@cinemalab.io>
2021-09-02 17:13:52 +0300

    Basic icon component implementation

--- components/lib/Icon/Icon.js
+++ components/lib/Icon/Icon.js
@@ -1,20 +1,50 @@
-import React, { useContext } from 'react'
-import { OutlinesContext } from '../../../context/outlinesContext'
-import { getHandler } from '../../../core/functions/components';
-import { IconBody } from './IconStyled';
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { onClick, onDragEnd, onDragEnter, onDragLeave, onDragOver, onDragStart, onDrop } from '../../../core/functions/actions';
+import { getComponent } from '../../../core/functions/components';
+import { IconComponent } from './IconStyled';
+
 
 export default function Icon(props) {
 
-    const outlines = useContext(OutlinesContext);
+    const id = props.componentData.id;
+    const activeComponent = useSelector(state => state.document.activeComponent);
+    const isActiveComponent = activeComponent && activeComponent.id === props.componentData.id;
+    const componentsData = useSelector(state => state.document.componentsData);
+    const componentData = getComponent(componentsData, id);
+    const dragendComponent = useSelector(state => state.document.dragendComponent);
+    const dispatch = useDispatch();
+    const state = useSelector(state => state.document);
+
+    const component = {
+        id,
+        state,
+        componentsData,
+        componentData,
+        activeComponent,
+        dragendComponent,
+        isDropBox: false,
+        dispatch
+    }
+
 
     return (
-        <IconBody
-            {...props} 
-            {...props.componentData} 
-            showOutlines={outlines}
-            onClick={getHandler(props, 'onClick')}
+        <IconComponent
+            id={id}
+            draggable
+            {...props}
+            componentData={componentData}
+            isActiveComponent={isActiveComponent}
+            onClick={(e) => onClick(e, component)}
+            onDragStart={(e) => onDragStart(e, component)}
+            onDragEnter={(e) => onDragEnter(e, component)}
+            onDragLeave={(e) => onDragLeave(e, component)}
+            onDragOver={(e) => onDragOver(e, component)}
+            onDragEnd={(e) => onDragEnd(e, component)}
+            onDrop={(e) => onDrop(e, component)}
         >
             {props.children}
-        </IconBody>
-    )
+        </IconComponent>
+    );
 }
--- components/lib/Icon/IconStyled.js
+++ components/lib/Icon/IconStyled.js
@@ -1,40 +1,14 @@
 import styled, {css} from 'styled-components';
-import { enableOutlines } from '../../../core/functions/outlines';
-import { colors } from '../../../core/variables/variables';
+import { getOutlines } from '../../../core/functions/outlines';
 import { StylesProvider } from '../../styles';
 
 
-// const defaultStyles = () => {
-//     return css`
-//         position: ${styles && styles.position || 'absolute'};
-//         width: ${styles && styles.width || '30px'};
-//         height: ${styles && styles.height || '30px'};
-//         background-repeat: ${styles && styles.backgroundRepeat || 'no-repeat'};
-//         background-color: ${styles && styles.backgroundColor || ''};
-//         background-size: ${styles && styles.backgroundSize || '100% auto'};
-//         background-position-x: ${styles && styles.backgroundPositionX || '50%'};
-//         background-position-y: ${styles && styles.backgroundPositionY || '50%'};
-//         background-image: ${styles && styles.backgroundImage || 'none'};
-
-//         &:before {
-//             position: ${styles && styles.position || 'absolute'};
-//             top: ${styles && styles.top || '0'};
-//             left: ${styles && styles.left || '0'};
-//         }
-//     `
-// }
-
-export const IconBody = styled.div`
-    position: absolute;
-    width: 30px;
-    height: 30px;
-    ${props => enableOutlines(props.showOutlines, colors.outline_icon)}
-
+export const IconComponent = styled.div`
     ${props => {
-        const styles = props.componentData.styles || null;
-
+        const styles = props.componentData.styles && props.componentData.styles.common || null;
         return css`
             ${styles && StylesProvider(styles)}
+            ${props => props.isActiveComponent && getOutlines()}
             ${styles && styles.isActive && StylesProvider(styles.isActive)}
         `
     }}
