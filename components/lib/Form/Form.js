80c9cfd67121e16fc047f103859cf5021c9501b8
m.ivanov <M.ivanov@cinemalab.io>
2021-09-02 18:06:06 +0300

    Basic form component implementation

--- components/lib/Form/Form.js
+++ components/lib/Form/Form.js
@@ -1,22 +1,82 @@
-import React, { useContext } from 'react'
-import { OutlinesContext } from '../../../context/outlinesContext'
-import { getAttrs } from '../../../core/functions/styles';
-import { FormBody } from './FormStyled'
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { onClick, onDragEnd, onDragEnter, onDragLeave, onDragOver, onDragStart, onDrop } from '../../../core/functions/actions';
+import { getChild, getComponent } from '../../../core/functions/components';
+import { FormComponent } from './FormStyled'
+
 
 
 export default function Form(props) {
 
-    const outlines = useContext(OutlinesContext);
-    const attrs = getAttrs(props.componentData);
+    const id = props.componentData.id;
+    const activeComponent = useSelector(state => state.document.activeComponent);
+    const isActiveComponent = activeComponent && activeComponent.id === id;
+    const componentsData = useSelector(state => state.document.componentsData);
+    const componentData = getComponent(componentsData, id);
+    const dragendComponent = useSelector(state => state.document.dragendComponent);
+    const state = useSelector(state => state.document);
+    const dispatch = useDispatch();
+
+    const [allowDrop, setAllowDrop] = useState(false);
+    const [dragCounter, setDragCounter] = useState(0);
+
+    const component = {
+        id,
+        state,
+        componentsData,
+        componentData,
+        activeComponent,
+        dragendComponent,
+        isDropBox: true,
+        allowDrop,
+        setAllowDrop,
+        setDragCounter,
+        dispatch
+    }
+
+    const checkAllowDrop = (dragendComponent, dropTarget) => {
+        if (getChild(dragendComponent, dropTarget.id)) {
+            return false;
+        }
+        if (dragendComponent.typeName === 'page') {
+            return false;
+        }
+        return true;
+    }
+
+    useEffect(() => {
+        if (dragendComponent) {
+            if (dragCounter === 0) {
+                setAllowDrop(false);
+            }
+            else if (dragendComponent) {
+                setAllowDrop(checkAllowDrop(dragendComponent, componentData));
+            }
+        }
+    }, [dragCounter, dragendComponent, componentData]);
+
 
     return (
-        <FormBody 
-            componentData={props.componentData} 
-            showOutlines={outlines}
-            action={attrs && attrs.action || ''}
-            method={attrs && attrs.method || ''}    
+        <FormComponent
+            {...props}
+            id={id}
+            componentData={componentData}
+            onMouseEnter={props.onMouseEnter}
+            onClick={(e) => onClick(e, component)}
+            isActiveComponent={isActiveComponent}
+            onDragStart={(e) => onDragStart(e, component)}
+            onDragEnter={(e) => onDragEnter(e, component)}
+            onDragLeave={(e) => onDragLeave(e, component)}
+            onDragOver={(e) => onDragOver(e, component)}
+            onDragEnd={(e) => onDragEnd(e, component)}
+            onDrop={(e) => onDrop(e, component)}
+            draggable
+            allowDrop={allowDrop}
         >
             {props.children}
-        </FormBody>
+        </FormComponent>
     )
 }
--- components/lib/Form/FormStyled.js
+++ components/lib/Form/FormStyled.js
@@ -1,43 +1,19 @@
 import styled, {css} from 'styled-components';
-import {enableOutlines} from '../../../core/functions/outlines';
-import {getStyles} from '../../../core/functions/styles';
-import { colors } from '../../../core/variables/variables';
+import { getOutlines } from '../../../core/functions/outlines';
+import { StylesProvider } from '../../styles';
 
 
-export const FormBody = styled.form`
 
+export const FormComponent = styled.form`
     ${props => {
-        const styles = getStyles(props.componentData);
+        const styles = props.componentData.styles && props.componentData.styles.common || null;
 
         return css`
-            ${enableOutlines(props.showOutlines, colors.outline_form)};
-            align-self: ${styles && styles.alignSelf || ''};
-
-            width: ${styles && styles.width || ''};
-            min-width: ${styles && styles.minWidth || '150px'};
-            min-height: ${styles && styles.minHeight || '200px'};
-
-            margin-top: ${styles && styles.marginTop || '0'};
-            margin-right: ${styles && styles.marginRight || '0'};
-            margin-bottom: ${styles && styles.marginBottom || '30px'};
-            margin-left: ${styles && styles.marginLeft || '0'};
-
-            padding-top: ${styles && styles.paddingTop || '15px'};
-            padding-right: ${styles && styles.paddingRight || '15px'};
-            padding-bottom: ${styles && styles.paddingBottom || '15px'};
-            padding-left: ${styles && styles.paddingLeft || '15px'};
-
-            border: ${styles && styles.border || 'none'};
-            border-top: ${styles && styles.borderTop || ''};
-            border-right: ${styles && styles.borderRight || ''};
-            border-bottom: ${styles && styles.borderBottom || ''};
-            border-left: ${styles && styles.borderLeft || ''};
-            border-radius: ${styles && styles.borderRadius || '4px'};
-
-            box-shadow: ${styles && styles.boxShadow || '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)'};
-            overflow: ${styles && styles.overflow || 'hidden'};
-
-            background-color: ${styles && styles.backgroundColor || '#ffffff'};
+            ${styles && StylesProvider(styles)}
+            ${props => props.isActiveComponent && getOutlines()}
+            ${props => props.allowDrop && `
+                outline: 2px solid #42a5f5;
+            `}
         `
     }}
 `;
