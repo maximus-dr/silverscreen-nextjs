5cc00f1f080cca98e6b4d463b5bdb50de3813f32
Max Ivanov <maximus-dr@mail.ru>
2021-10-04 07:52:10 +0300

    Update page data function

--- pages/afisha/[event]/index.js
+++ pages/afisha/[event]/index.js
@@ -3,15 +3,18 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import EventPageComponent from '../../../components/test/EventPage/EventPageComponent';
 import { fetchDataList } from '../../../core/functions/common/common';
+import { renderComponents } from '../../../core/functions/render';
 import { setDataList } from '../../../store/actions/data';
 import { setDocumentComponentsData } from '../../../store/actions/document';
 import { initializeStore } from '../../../store/store';
+import { updatePageData } from './../../preview/index';
 const path = require('path');
 const fs = require('fs');
 
 
 
-export async function getServerSideProps() {
+
+export async function getServerSideProps({req, resolvedUrl}) {
 
     const reduxStore = initializeStore()
     const { dispatch } = reduxStore
@@ -21,6 +24,13 @@ export async function getServerSideProps() {
     const componentsData = JSON.parse(data);
 
     const dataList = fetchDataList(fs, path);
+    
+    const params = resolvedUrl.split('/');
+    const eventId = params[params.length - 1];
+    const event = dataList.events.find(event => event.id === eventId);
+
+    updatePageData(componentsData, dataList.events, event);
+    
 
     dispatch(setDataList(dataList));
     dispatch(setDocumentComponentsData(componentsData));
@@ -38,13 +48,18 @@ export default function EventPage() {
 
     const route = useRouter();
     const state = useSelector(state => state);
+    const {componentsData} = state.document;
     const {events, shows} = state.data;
 
     const eventId = route.query.event;
     const event = events.find(item => item.id === eventId);
     const eventShows = shows.filter(show => show.eventId === eventId);
 
+    const components = renderComponents(componentsData, state);
+
     return (
-        <EventPageComponent event={event} eventShows={eventShows} />
+        <>
+            {components}
+        </>
     )
 }
--- pages/preview/index.js
+++ pages/preview/index.js
@@ -11,6 +11,74 @@ import { initializeStore } from '../../store/store';
 const fs = require('fs');
 
 
+export const updatePageData = (component, events, event) => {
+    if (component.role === 'eventsContainer') {
+        const card = component.childrenList.find(child => child.role === 'card');
+        
+        const createNewCard = (card, event) => {
+
+            const updateCardData = (cardElement) => {
+                if (cardElement.role === 'card') {
+                    cardElement.eventId = event.id;
+                }
+                if (cardElement.role === 'posterLink') {
+                    cardElement.link = `/afisha/${event.id}`
+                    cardElement.styles.common.backgroundImage = `url('` + event.posterLink + `')`;
+                }
+                if (cardElement.role === 'eventTitle') {
+                    cardElement.value = event.acronym;
+                }
+                if (cardElement.role === 'genreList') {
+                    const genreItem = cardElement.childrenList.find(item => item.role === 'genreItem');
+
+                    const genreItems = event.genre.map(genre => {
+                        const copy = JSON.parse(JSON.stringify(genreItem));
+                        copy.value = genre.acronym;
+                        return copy;
+                    });
+                    cardElement.childrenList = genreItems;
+                }
+                if (cardElement.childrenList && cardElement.childrenList.length > 0) {
+                    cardElement.childrenList.forEach(child => {
+                        updateCardData(child);
+                    });
+                }
+                return cardElement;
+            }
+            const template = JSON.parse(JSON.stringify(card));
+            const newCard = updateCardData(template);
+            
+            return newCard;
+        }
+        const cards = events.map(event => {
+            const newCard = createNewCard(card, event);
+            
+            return updateComponentIds(newCard);
+        });
+
+
+        component.childrenList = cards;
+    }
+
+    if (component.role === 'eventTitle') {
+        component.value = event.acronym;
+    }
+
+    if (component.role === 'poster') {
+        component.styles.common.backgroundImage = `url('` + event.posterLink + `')`;
+    }
+
+    if (component.role === 'eventBackground') {
+        component.styles.common.backgroundImage = `url('` + event.posterLink + `')`;
+    }
+
+    if (component.childrenList && component.childrenList.length > 0) {
+        component.childrenList.forEach(child => updatePageData(child, events, event));
+    }
+}
+
+
+
 export async function getServerSideProps() {
     const reduxStore = initializeStore();
     const {dispatch} = reduxStore;
@@ -44,66 +112,7 @@ export default function PreviewPage() {
 
         if (!componentsData) {
             const data = JSON.parse(localStorage.getItem('page_data'));
-
-            const updatePageData = (component) => {
-                if (component.role === 'eventsContainer') {
-                    const card = component.childrenList.find(child => child.role === 'card');
-                    
-                    const createNewCard = (card, event) => {
-
-                        const updateCardData = (cardElement) => {
-                            if (cardElement.role === 'card') {
-                                cardElement.eventId = event.id;
-                            }
-                            if (cardElement.role === 'posterLink') {
-                                cardElement.link = `/afisha/${event.id}`
-                                cardElement.styles.common.backgroundImage = `url('` + event.posterLink + `')`;
-                            }
-                            if (cardElement.role === 'eventTitle') {
-                                cardElement.value = event.acronym;
-                            }
-                            if (cardElement.role === 'genreList') {
-                                const genreItem = cardElement.childrenList.find(item => item.role === 'genreItem');
-
-                                const genreItems = event.genre.map(genre => {
-                                    const copy = JSON.parse(JSON.stringify(genreItem));
-                                    copy.value = genre.acronym;
-                                    return copy;
-                                });
-                                cardElement.childrenList = genreItems;
-                            }
-                            if (cardElement.childrenList && cardElement.childrenList.length > 0) {
-                                cardElement.childrenList.forEach(child => {
-                                    updateCardData(child);
-                                });
-                            }
-                            return cardElement;
-                        }
-                        const template = JSON.parse(JSON.stringify(card));
-                        const newCard = updateCardData(template);
-                        
-                        return newCard;
-                    }
-
-
-
-
-                    const cards = events.map(event => {
-                        const newCard = createNewCard(card, event);
-                        
-                        return updateComponentIds(newCard);
-                    });
-
-
-                    component.childrenList = cards;
-                }
-
-                if (component.childrenList && component.childrenList.length > 0) {
-                    component.childrenList.forEach(child => updatePageData(child));
-                }
-            }
-
-            updatePageData(data);
+            updatePageData(data, events);
             dispatch(setDocumentComponentsData(data));
         }
     });
